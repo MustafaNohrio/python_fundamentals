035543e761d01e48858a189420d9c747cace48f2
Mustafa nohrio <gh.mustafanohrio@gmial.com>
2026-07-03 19:29:54 +0500

    Fixed the bug in Variance.py

--- week01/Mini_Projects/variance_SD.py
+++ week01/Mini_Projects/variance_SD.py
@@ -2,15 +2,14 @@ data = [2, 4, 6, 8]
 
 mean = sum(data) / len(data)
 
-variane = 0
-
+total = 0
 for i in data:
-    sum = (i - mean) **2   #sum of squared distance of each data point from mean
+    total += (i - mean) **2   #sum of squared distance of each data point from mean
 
-variane = sum / len(data)   #average of squared sum
+variane = total / len(data)   #average of squared sum
 
 sd = variane **0.5  #square root of variance
 
 print('mean', mean)
 print('varince', variane)
-print('stndard deviation', sd)
+print('standard deviation', sd)
